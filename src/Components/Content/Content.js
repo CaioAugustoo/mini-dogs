8f148415d81f0c9c77c17d7c8e8d1402dd640ea4
caioaugustoo <caioamfr@gmail.com>
2021-03-01 14:34:53 -0300

    Criado componente para renderizar o conteudo

---
+++ src/Components/Content/Content.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { useSelector } from "react-redux";
+
+import Loading from "../Helper/Loading";
+import Login from "../Login/Login";
+import Photos from "../Photos/Photos";
+
+const Content = () => {
+  const { user, token } = useSelector(state => state.login);
+
+  if (user.loading || token.loading) return <Loading />;
+  if (user.data) return <Photos />;
+  else return <Login />;
+};
+
+export default Content;
